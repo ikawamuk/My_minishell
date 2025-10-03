2818698da3dca2b661fd62929f102361abf15f59
ikawamuk <ikawamuk@student.42.fr>
2025-10-03 09:51:52 +0900

    update:

--- include/sig.h
+++ include/sig.h
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   sig.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kokurita <kokurita@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ikawamuk <ikawamuk@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 15:09:20 by ikawamuk          #+#    #+#             */
-/*   Updated: 2025/09/29 20:38:18 by kokurita         ###   ########.fr       */
+/*   Updated: 2025/10/03 09:51:48 by ikawamuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIG_H
 # define SIG_H
+
+# include <signal.h>
 
 extern volatile sig_atomic_t	g_sig;
 
